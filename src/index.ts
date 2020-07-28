31e4c0dc49ad3db08bc1b2ae60497ee32b545ac5
George <georgehm@pm.me>
2020-07-28 10:50:49 +0100

    Refactor index to have basic timer functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
-async function main(): Promise<void> {
-    console.log('hello world!');
+class Allotment {
+    private storedTime: number;
+
+    private active: boolean;
+
+    private lastRecordedTime: number;
+
+    private timeToCalculateFrom: number|null;
+
+    private constructor() {
+        this.storedTime = Date.now();
+        this.lastRecordedTime = 0;
+        this.active = true;
+        this.timeToCalculateFrom = null;
+    }
+
+    public static start() {
+        return new Allotment();
+    }
+
+    public start() {
+        this.active = true;
+        return true;
+    }
+
+    public stop() {
+        this.active = false;
+        this.lastRecordedTime = this.time;
+        return true;
+    }
+
+    public get time() {
+        if (!this.active) {
+            return this.lastRecordedTime;
+        }
+        const timeNow: number = this.timeToCalculateFrom || Date.now();
+        return (timeNow - this.storedTime) + this.lastRecordedTime;
+    }
 }
 
-export default main;
+export default Allotment.start;
